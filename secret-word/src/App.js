cd98e1003f80948318b4dbcaaee203507db2d2cd
Matheus de Farias Cavalcanti Santos <matheusdefariascs@gmail.com>
2023-09-05 16:55:54 -0300

    feat: implemented the initial transitions and flows among the stages of the game

---
+++ secret-word/src/App.js
@@ -0,0 +1,50 @@
+// CSS
+import './App.css';
+
+// React
+import { useCallback, useEffect, useState } from 'react';
+
+// Data
+import {wordsList} from './data/words';
+
+// Components
+import StartScreen from './components/StartScreen';
+import GameOver from './components/GameOver';
+import Game from './components/Game';
+
+const stages = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
+
+function App() {
+
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  // Starts the Scret Word Game
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  // Process the letter input
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  // Restarts the game
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
+
+  return (
+    <div className="App">
+      {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+      {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === 'end' && <GameOver retry={retry}/>}
+    </div>
+  );
+}
+
+export default App;
